4a154c4025a41a542f331ee7d36e4bc55c9868d4
luisolguin <luisolguinjh@hotmail.com>
2021-06-10 23:43:32 +0000

    Cambios verificados en Centos

--- install-all.sh
+++ install-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo yum update
-sudo yum install git docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker
-
 sudo systemctl status docker
-
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+sudo docker-compose --version
+
